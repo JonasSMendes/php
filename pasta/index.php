6a3646a95e63b3212a6a4c076bb44d096a4b11cd
jonas mendes <jonas.jsm2903@gmail.com>
2023-03-28 10:47:49 -0300

    php

---
+++ pasta/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php echo 'teste 1' ?>
+
+ <br>
+
+<?php echo 'teste 2' #eita nois ?>
+
+ <br>
+
+<?php echo "teste 3" //ola pessoa?>
+ 
+
+
+</body>
+</html>
