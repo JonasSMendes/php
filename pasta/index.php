8dfdc7757fa41c78c782d61ec372c8e9dc516c44
jonas mendes <jonas.jsm2903@gmail.com>
2023-03-29 14:15:31 -0300

    variáveis e operadores php

--- pasta/index.php
+++ pasta/index.php
@@ -1,3 +1,45 @@
+<?php 
+
+// maneira correta de escrever php variavel usar a mesma forma no codigo inteiro
+/*
+$name  = 'jonas';
+$variavel = 100;
+$var1 = 0;
+$var_um = 0;
+$varUm = 0;
+$_um = 0;
+$_1 = 0;
+
+// varievais tem muitos tipos de valores 
+
+$x = 100 % 3; //resto de operação de divisão 
+$x = 4** 2; // permite calcular os espoentes de uma variavel
+
+
+echo $x;
+
+echo "<br>";
+
+
+
+
+$a = 100;
+$b = 30;
+$resultado = $a + $b ;//130
+$resultado = $a + 100; //230
+$resultado = $a * $b; //2,300
+
+$resultado = 1000 + $a + 10 + $b;
+
+
+
+*/
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,18 +49,48 @@
     <title>Document</title>
 </head>
 <body>
-    
-<?php echo 'teste 1' ?>
-
- <br>
-
-<?php echo 'teste 2' #eita nois ?>
-
- <br>
-
-<?php echo "teste 3" //ola pessoa?>
  
+<?php
 
+//exercicios de aritmetica
+
+/*
+$a = 100;
+echo $a;
+echo '<br>';
+*/
+
+/*
+$a = 1000;
+$b = $a / 2;
+echo $b;
+*/
+
+/*
+$a = 100;
+$b = $a * 3;
+echo $b
+*/
+
+/*
+$a = 20;
+echo $a;
+echo '<br>';
+$a++;
+echo $a;
+*/
+
+/*
+$a = 100;
+$b = 250;
+$c = $a + $b;
+echo ++$c
+*/
+
+
+
+
+?>
 
 </body>
 </html>
