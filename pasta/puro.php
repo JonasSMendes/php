15d121efabb72ecd406a3775f31a2ff4f4e3e109
jonas mendes <jonas.jsm2903@gmail.com>
2023-03-30 13:33:26 -0300

    exercicio string

--- pasta/puro.php
+++ pasta/puro.php
@@ -1,3 +1,8 @@
 <?php
 
 echo "ola uhuu\n\rteste";
+
+
+$frase = 'ola eu sou o maior progrmador de php';
+echo '<br>';
+
---
+++ pasta/testes.php
@@ -0,0 +1,118 @@
+<?php 
+
+// maneira correta de escrever php variavel usar a mesma forma no codigo inteiro
+
+/*
+
+$name  = 'jonas';
+$variavel = 100;
+$var1 = 0;
+$var_um = 0;
+$varUm = 0;
+$_um = 0;
+$_1 = 0;
+
+// varievais tem muitos tipos de valores 
+
+$x = 100 % 3; //resto de operação de divisão 
+$x = 4** 2; // permite calcular os espoentes de uma variavel
+
+
+echo $x;
+
+echo "<br>";
+
+
+
+
+$a = 100;
+$b = 30;
+$resultado = $a + $b ;//130
+$resultado = $a + 100; //230
+$resultado = $a * $b; //2,300
+
+$resultado = 1000 + $a + 10 + $b;
+
+
+
+
+
+// no php 7 foi adicionado um novo operador de comparação
+// desligando por spaceship operador 
+
+//<=>
+
+// se os valores são iguais é = 0
+// se os valores são A maior que B = 1
+//se os valores são A menor que B = -1
+*/
+
+/*
+$a = 'jonas';
+$b = 'pedro';
+$todo_nome = "$a $b";
+
+echo $todo_nome;
+*/
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ 
+<?php
+
+//exercicios de aritmetica
+
+/*
+$a = 100;
+echo $a;
+echo '<br>';
+*/
+
+/*
+$a = 1000;
+$b = $a / 2;
+echo $b;
+*/
+
+/*
+$a = 100;
+$b = $a * 3;
+echo $b
+*/
+
+/*
+$a = 20;
+echo $a;
+echo '<br>';
+$a++;
+echo $a;
+*/
+
+/*
+$a = 100;
+$b = 250;
+$c = $a + $b;
+echo ++$c
+*/
+
+
+
+
+?>
+
+</body>
+</html>
