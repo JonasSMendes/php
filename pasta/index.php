eb671ee33ae902897cbeffb7f340b123ce879899
jonas mendes <jonas.jsm2903@gmail.com>
2023-04-01 09:28:09 -0300

    arrays

--- pasta/index.php
+++ pasta/index.php
@@ -48,7 +48,65 @@ $numero_letras = mb_strlen($nome_completo)
 <p>
     <?php echo "meu nome é".' '. strtoupper($nome); echo " meu sobrenome é".' '.strtoupper($sobrenome); ?>
 </p>
+<br>
+<?php
+// arrays e seus modos de usar no php
+$dados = [
+    100,
+    200,
+];
 
+echo $dados[1];
+
+$dados[] = 300;
+
+echo '<br>';
+
+echo $dados[2];
+echo '<br>';
+
+$cadastro = [
+    'nome' => 'jonas',
+    'idade'=> '25',
+    'email' => 'jonas@gmail.com',
+    'telefone' => '99999999'
+];
+
+echo $cadastro['telefone'];
+
+echo '<br>';
+
+$multi = [
+    'brasil' => ['basilia', 'rio de janeiro', 'são paulo'],
+    'portugal' => ['lisboa', 'coimbra', 'porto']
+];
+
+echo $multi['portugal'][1];
+echo '<br>';
+$cake = 'torta';
+
+
+if('bolo' === $cake){
+    echo 'que delicia seu bolo';
+} else if('torta' === $cake){
+    echo 'hmmm não sei oque é isso';
+}
+echo '<br>';
+
+$opcao = 0; // false
+
+$name = $opcao == 1 ? 'jonas' : 'pedro';
+
+echo $name;
+
+echo '<br>';echo '<br>';
+
+
+$color = 1;
+
+?>
+
+<h2 style="color: <?= $color == 0 ? 'red' : 'blue'?>"> esse texto foi mudado com php</h2>
 
 </body>
 </html>
--- pasta/puro.php
+++ pasta/puro.php
@@ -6,3 +6,31 @@ echo "ola uhuu\n\rteste";
 $frase = 'ola eu sou o maior progrmador de php';
 echo '<br>';
 
+
+$dados = [
+    10 => 100,
+    20 => 200,
+];
+
+// if else 
+
+$cake = 'torta';
+
+if('bolo' === $cake){
+    echo 'que delicia seu bolo';
+} else if('torta' === $cake){
+    echo 'hmmm não sei oque é isso';
+}
+
+
+//operação ternario
+
+$opcao = 0;
+
+$name = $opcao == 1 ? 'jonas' : 'pedro';
+
+echo $name;
+
+$color = 1;
+
+//<h2 style="color: <?= $color == 0 ? 'red' : 'blue'?>"> esse texto foi mudado com php</h2>
